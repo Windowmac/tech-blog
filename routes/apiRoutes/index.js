ac0061f068f87e669b7b3ddbbe7aace169baa16a
McKendree Strommer <mckendree.strommer@gmail.com>
2021-08-15 12:28:57 -0700

    added user model to db then added routes to server to get model info

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const userRoutes = require('./users.js');
 
 router.get('/', (req, res) => {
     res.json('got here!');
 });
+
+router.use('/users', userRoutes);
 
 module.exports = router;
