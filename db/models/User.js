1f856525ff34dd5830ef651f54b7a435558299cf
McKendree Strommer <mckendree.strommer@gmail.com>
2021-08-15 13:09:53 -0700

    added user validation for hashed password and route to check it

--- db/models/User.js
+++ db/models/User.js
@@ -2,24 +2,31 @@ const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../connection.js');
 const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+  validatePassword(password) {
+    return bcrypt.compare(password, this.password);
+  }
+}
 
-User.init({
+User.init(
+  {
     username: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-},
-{ sequelize, modelName: 'user', freezeTableName: true, timestamps: false}
+  },
+  { sequelize, modelName: 'user', freezeTableName: true, timestamps: false }
 );
 
-User.beforeCreate(async (user, options) => {
-    const hashedPassword = await bcrypt.hash(user.password, 10);
-    user.password = hashedPassword;
+User.beforeCreate(async (user) => {
+  const hashedPassword = await bcrypt.hash(user.password, 10).catch((err) => {
+    console.error(err);
   });
+  user.password = hashedPassword;
+});
 
 module.exports = User;
--- routes/apiRoutes/users.js
+++ routes/apiRoutes/users.js
@@ -4,13 +4,21 @@ const User = require('../../db/models/User.js');
 //end path: /api/users
 
 router.get('/', async (req, res) => {
-    const users = await User.findAll()
-    res.json(users);
+    const users = await User.findAll().catch(err => {res.status(500).json('unable to find users')});
+    res.status(200).json(users);
 });
 
 router.post('/', async (req, res) => {
     const newUser = await User.create(req.body).catch(err => {res.status(500).json('unable to create user')});
     res.status(201).json(newUser);
 });
+
+router.get('/sign-in/:username', async (req, res) => {
+    const user = await User.findOne({ where: { username: req.params.username } }).catch(err => {
+      res.status(500).json('error finding users :(')});;
+    const validated = await user.validatePassword(req.body.password);
+    console.log('validated variable is: ', validated);
+    res.status(202).json(validated);
+  });
 
 module.exports = router;
