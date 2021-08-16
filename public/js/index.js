19a6df449f95c75f5bc860492854c09bcb73b0ca
McKendree Strommer <mckendree.strommer@gmail.com>
2021-08-16 11:03:39 -0700

    login displays homepage, create user displays homepage, landing page cleaned up

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,101 @@
 
 
 
+const chooseLoginEl = document.getElementById('choose-login');
+const chooseCreateEl = document.getElementById('choose-create');
 
-const submitBtnEl = document.getElementById('submit-btn');
-const passwordEl = document.getElementById('password');
-const usernameEl = document.getElementById('username');
+const displayLogIn = (event) => {
+  chooseLoginEl.parentNode.removeChild(chooseLoginEl);
+  chooseCreateEl.parentNode.removeChild(chooseCreateEl);
 
-const createUser = async (event) => {
-    event.preventDefault();
+  const loginForm = document.createElement('form');
+  const usernameInput = document.createElement('input');
+  const passwordInput = document.createElement('input');
+  const usernameLabel = document.createElement('label');
+  const passwordLabel = document.createElement('label');
+  const submitBtnEl = document.createElement('input');
+  submitBtnEl.type = 'submit';
+  usernameLabel.textContent = 'username: ';
+  passwordLabel.textContent = 'password: ';
+
+  loginForm.appendChild(usernameLabel);
+  loginForm.appendChild(usernameInput);
+  loginForm.appendChild(passwordLabel);
+  loginForm.appendChild(passwordInput);
+  loginForm.appendChild(submitBtnEl);
+
+  document.body.appendChild(loginForm);
+
+  const handleLogin = async (username, password) => {
 
     const body = {
-        username: usernameEl.value,
-        password: passwordEl.value,
+        username: username,
+        password: password,
     };
     
-        console.log('body');
+        console.log(body);
+
+    const result = await fetch(`/api/users/sign-in/${username}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        return result;
+      });
+   
+    if(result){
+        window.location.href = `/${body.username}`;
+    } else {
+        alert('error with login')
+        window.location.href = '/';
+    }
+  }
+
+  submitBtnEl.addEventListener('click', (event) => {
+    event.preventDefault();
+    handleLogin(usernameInput.value, passwordInput.value)
+  });
+
+}
+
+chooseLoginEl.addEventListener('click', displayLogIn);
+
+const displayCreateUser = (event) => {
+  chooseLoginEl.parentNode.removeChild(chooseLoginEl);
+  chooseCreateEl.parentNode.removeChild(chooseCreateEl);
+
+  const createForm = document.createElement('form');
+  const usernameInput = document.createElement('input');
+  const passwordInput = document.createElement('input');
+  const usernameLabel = document.createElement('label');
+  const passwordLabel = document.createElement('label');
+  const submitBtnEl = document.createElement('input');
+  submitBtnEl.type = 'submit';
+  usernameLabel.textContent = 'username: ';
+  passwordLabel.textContent = 'password: ';
+
+  createForm.appendChild(usernameLabel);
+  createForm.appendChild(usernameInput);
+  createForm.appendChild(passwordLabel);
+  createForm.appendChild(passwordInput);
+  createForm.appendChild(submitBtnEl);
+
+  document.body.appendChild(createForm);
+
+  const handleCreateUser = async (username, password) => {
+
+    const body = {
+        username: username,
+        password: password,
+    };
+    
+        console.log(body);
+
         const result = await fetch('/api/users', {
           method: 'POST',
           headers: {
@@ -36,5 +117,10 @@ const createUser = async (event) => {
         }
 }
 
-submitBtnEl.addEventListener('click', createUser);
+  submitBtnEl.addEventListener('click', (event) => {
+    event.preventDefault();
+    handleCreateUser(usernameInput.value, passwordInput.value)
+  });
+}
 
+chooseCreateEl.addEventListener('click', displayCreateUser);
--- routes/apiRoutes/users.js
+++ routes/apiRoutes/users.js
@@ -4,34 +4,45 @@ const User = require('../../db/models/User.js');
 //end path: /api/users
 
 router.get('/', async (req, res) => {
-    const users = await User.findAll().catch(err => {res.status(500).json('unable to find users')});
-    res.status(200).json(users);
+  const users = await User.findAll().catch((err) => {
+    res.status(500).json('unable to find users');
+  });
+  res.status(200).json(users);
 });
 
 router.post('/', async (req, res) => {
-    const newUser = await User.create(req.body).catch(err => {res.status(500).json('unable to create user')});
-    req.session.save(() => {
-        req.session.loggedIn = true;
-        res.status(201).json(newUser);
-    });
+  const newUser = await User.create(req.body).catch((err) => {
+    res.status(500).json('unable to create user');
+  });
+  req.session.save(() => {
+    req.session.loggedIn = true;
+    res.status(201).json(newUser);
+  });
 });
 
 router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
-router.get('/sign-in/:username', async (req, res) => {
-    const user = await User.findOne({ where: { username: req.params.username } }).catch(err => {
-      res.status(500).json('error finding users :(')});;
-    const validated = await user.validatePassword(req.body.password);
-    console.log('validated variable is: ', validated);
+router.post('/sign-in/:username', async (req, res) => {
+  const user = await User.findOne({
+    where: { username: req.params.username },
+  }).catch((err) => {
+    res.status(500).json('error finding users :(');
+  });
+  const validated = await user.validatePassword(req.body.password).catch(err => {
+    res.status(404).json('invalid login');
+  });
+  req.session.save(() => {
+    req.session.loggedIn = true;
     res.status(202).json(validated);
   });
+});
 
 module.exports = router;
