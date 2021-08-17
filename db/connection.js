347d3da595c288b780f2458caf90027f67fb1eaa
McKendree Strommer <mckendree.strommer@gmail.com>
2021-08-17 10:31:33 -0700

    added port to sequelize connection

--- db/connection.js
+++ db/connection.js
@@ -6,8 +6,9 @@ const sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USERNAME, 
     process.env.DB_PASSWORD, {
-    host: process.env.JAWSDB_URL,
-    dialect: 'mysql'
+    host: process.env.JAWSDB_GREEN_URL,
+    dialect: 'mysql',
+    port: 3306
   });
 
 module.exports = sequelize;
